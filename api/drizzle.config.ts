458d30b456bdc6ae22b17170aac378bf24e5b425
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gamil.com>
2023-07-04 03:23:57 +0330

    feat: add drizzle studio settings

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "drizzle-kit";
 export default {
   schema: "./src/schema/*.ts",
   out: "./drizzle",
-  driver: "libsql",
+  driver: "better-sqlite",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
