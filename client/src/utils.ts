600ca1d12d9ec890352b042fee404bca427c7575
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gamil.com>
2023-08-18 17:03:55 +0330

    feat: add eden treaty to the client

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -1,1 +1,5 @@
+import { type Server } from "../../api/src/index";
+import { edenTreaty } from "@elysiajs/eden";
+
 export const baseURL = "http://localhost:3000";
+export const api = edenTreaty<Server>(baseURL);
