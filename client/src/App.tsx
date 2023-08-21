161286ec5f75d89287d2640320ea1509d8bbbbe2
Mostafa Zeighami <isowz2000@gmail.com>
2023-08-21 20:16:20 +0330

    added router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,29 @@
 import './App.css'
 import MainPage from './components/MainPage'
+import MainPage2 from './components/MainPage2'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/home",
+    element: <MainPage />,
+  },
+  {
+    path: "/",
+    element: <MainPage2 />,
+  },
+]);
 
 function App() {
   return(
     <>
-      <MainPage />
+      <RouterProvider router={router} />
     </>
   )
 }
 
 export default App
+
